de7d7537d6510a4432f11534a82aa4487a12f806
Dmitry <dimasel@gmail.com>
2025-06-11 22:08:27 +0700

    New project
    A new project has been created

---
+++ WBPartnersIOS/App/WBPartnersIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBPartnersIOSApp.swift
+//  WBPartnersIOS
+//
+//  Created by Dmitriy Eliseev on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct WBPartnersIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
